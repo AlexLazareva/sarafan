f01856f5d90c05e957f152ee6f0594e60848b285
Alex Lazareva <alex.lazareva25@yandex.ru>
2019-08-05 22:46:18 +0300

    include vue-resource to project

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -1,7 +1,12 @@
+var message = Vue.resource('/message{/id}');
+Vue.component('message-row', {
+    props: ['message'],
+    template: '<div> {{ message.text }} </div>'
+});
 Vue.component('messages-list', {
     props: ['messages'],
     template: '<div>' +
-            '<div v-for="message in messages">{{ message.text }}</div>' +
+            '<message-row v-for="message in messages" :key="message.id" :message="message"/>' +
         '</div>'
 });
 
