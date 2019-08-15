f0318381ef96d1b794f9ac1905803f1a745caeff
Alex Lazareva <alex.lazareva25@yandex.ru>
2019-08-15 16:14:55 +0300

    add User auth

---
+++ src/main/java/initcode/sarafan/domain/User.java
@@ -0,0 +1,79 @@
+package initcode.sarafan.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "auth_user")
+@Data
+
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private String picture;
+    private String email;
+    private String gender;
+    private String locale;
+    private LocalDateTime lastVisit;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserPic() {
+        return picture;
+    }
+
+    public void setUserPic(String picture) {
+        this.picture = picture;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+
+    public LocalDateTime getLastVisit() {
+        return lastVisit;
+    }
+
+    public void setLastVisit(LocalDateTime lastVisit) {
+        this.lastVisit = lastVisit;
+    }
+}
