26c1e310fe5fafd097eb787c87cf0ca8ff26c40b
Alex Lazareva <alex.lazareva25@yandex.ru>
2019-08-31 20:43:24 +0300

    fix socket

--- src/main/resources/static/scripts/js/main.js
+++ src/main/resources/static/scripts/js/main.js
@@ -3,7 +3,9 @@ import VueResorce from 'vue-resource';
 import App from 'pages/App.vue';
 import { connect} from "./util/websocket";
 
-connect();
+if(frontendData.profile) {
+    connect();
+}
 
 Vue.use(VueResorce);
 
