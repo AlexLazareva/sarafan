e4ab1bb1052dcc5d01868c6e81f4e432ed89d6c7
Alex Lazareva <alex.lazareva25@yandex.ru>
2019-09-24 22:23:09 +0300

    add store's mutations

--- src/main/resources/static/scripts/js/store/store.js
+++ src/main/resources/static/scripts/js/store/store.js
@@ -15,8 +15,41 @@ export default new Vuex.Store({
         }
     },
     mutations: {
-        increment (state) {
-            state.count++
+        addMessageMutation(state, message) {
+            state.messages = [
+                ...state.messages,
+                message
+            ];
+        },
+        updateMessageMutation(state, message) {
+            const updateIndex = state.messages.findIndex(item => item.id === message.id);
+
+            state.messages = [
+                ...state.messages.slice(0, updateIndex),
+                message,
+                ...state.messages.slice(updateIndex + 1)
+            ];
+        },
+        removeMessageMutation(state, message) {
+            const deletionIndex = state.messages.findIndex(item => item.id === message.id);
+
+            if (deletionIndex > -1) {
+                state.messages = [
+                    ...state.messages.slice(0, deletionIndex),
+                    ...state.messages.slice(deletionIndex + 1)
+                ];
+            }
+        }
+    },
+    actions: {
+        async addMessageAction({commit}, message) {
+
+        },
+        async updateMessageAction({commit}, message) {
+            // state.messages.push(message);
+        },
+        async removeMessageAction({commit}, message) {
+            // state.messages.push(message);
         }
     }
 });
